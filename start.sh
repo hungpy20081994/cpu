227b8c357266176f53e8583c61f1a28fff71aef3
hungpy20081994 <67790417+hungpy20081994@users.noreply.github.com>
2021-03-15 11:59:11 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ echo '$username:$password' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
  wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
  unzip -qq -n ngrok-stable-linux-amd64.zip
- ./ngrok authtoken  $authtoken && ./ngrok tcp 22 
+ ./ngrok authtoken  1eJ879JFo0Lde3KB9Z3E7R0q8vN_7CF88NU5Q1epjY9k5Nomr && ./ngrok tcp 22 
  done
