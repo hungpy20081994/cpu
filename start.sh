f12b530ccba90a7cbebf5d72c50ea233e8c9020b
hungpy20081994 <67790417+hungpy20081994@users.noreply.github.com>
2021-03-15 12:16:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 username = "user"
 password = "root"
-# Creation of user
-sudo useradd -m $username &> /dev/null
-# Add user to sudo group
-sudo adduser $username sudo &> /dev/nulll 
-# Set password of user to 'root'
-echo '$username:$password' | sudo chpasswd
-# Change default shell from sh to bash
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
+#Set root password
+echo root:$password | chpasswd
+mkdir -p /var/run/sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+print("username: root")
  wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
  unzip -qq -n ngrok-stable-linux-amd64.zip
- ./ngrok authtoken  1eJ879JFo0Lde3KB9Z3E7R0q8vN_7CF88NU5Q1epjY9k5Nomr && ./ngrok tcp 22 
+ ./ngrok authtoken  $authtoken && ./ngrok tcp 22 
  done
