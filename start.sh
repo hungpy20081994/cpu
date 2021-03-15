105fea325df078da20e7f13cdeb331308a179bfe
hungpy20081994 <67790417+hungpy20081994@users.noreply.github.com>
2021-03-15 12:46:15 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-
-
- wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
- unzip -qq -n ngrok-stable-linux-amd64.zip
- ./ngrok2 authtoken  1eJ879JFo0Lde3KB9Z3E7R0q8vN_7CF88NU5Q1epjY9k5Nomr
- ./ngrok2 tcp 22 
- done
