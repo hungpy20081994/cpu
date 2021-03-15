91c9f9d5e08d7ea7a21cf5240d4c0b13b6681b87
hungpy20081994 <67790417+hungpy20081994@users.noreply.github.com>
2021-03-15 12:37:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 
-print("username: root")
+
  wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
  unzip -qq -n ngrok-stable-linux-amd64.zip
  ./ngrok authtoken  $authtoken
- ./ngrok tcp 22 
+ ./ngrok 22
+ copy ngrok.exe C:\Windows\System32
+  run: .\ngrok.exe 1eJ879JFo0Lde3KB9Z3E7R0q8vN_7CF88NU5Q1epjY9k5Nomr 
  done
