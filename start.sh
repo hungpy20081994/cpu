98d43037d678d87e5d4df16034cab7cf1910c97a
hungpy20081994 <67790417+hungpy20081994@users.noreply.github.com>
2021-03-15 13:11:54 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,8 @@ wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 
 unzip -qq -n ngrok-stable-linux-amd64.zip
 
-./ngrok authtoken  $authtoken && ./ngrok tcp 22  
+./ngrok authtoken  $authtoken
+./ngrok tcp 22  
 
  done 
 
