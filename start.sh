8f7fc5f49c372d98cc67f6c55452c094a36cc482
hungpy20081994 <67790417+hungpy20081994@users.noreply.github.com>
2021-03-15 12:27:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
-username = "user"
-password = "root"
-apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
-#Set root password
-echo root:$password | chpasswd
-mkdir -p /var/run/sshd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
 print("username: root")
  wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
  unzip -qq -n ngrok-stable-linux-amd64.zip
